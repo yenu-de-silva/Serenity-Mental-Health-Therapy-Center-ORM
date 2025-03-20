9e43b43bf0739a930e8b3098760bec325991a436
himadi44 <himadi2024@icloud.com>
2025-03-20 20:16:30 +0530

    case 03 updated

---
+++ src/main/java/lk/ijse/gdse/exception/PaymentFailedException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.exception;
+
+public class PaymentFailedException extends RuntimeException {
+    public PaymentFailedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lk/ijse/gdse/exception/ScheduleConflictException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.exception;
+
+public class ScheduleConflictException extends RuntimeException {
+    public ScheduleConflictException(String message) {
+        super(message);
+    }
+}
