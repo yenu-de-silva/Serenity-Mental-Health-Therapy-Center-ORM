97898d05450a085a579fe64e9460111a969b6fa3
himadi44 <himadi2024@icloud.com>
2025-03-20 19:51:24 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/Launcher.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse;
+
+public class Launcher {
+}
---
+++ src/main/java/lk/ijse/gdse/Main.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
